1bd1faf30b5d63c9821eda7f79e9ff0c7cdc9ded
Your Name <you@example.com>
2019-07-31 11:05:32 +0700

    update

--- app/src/main/java/com/example/kotlin_1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_1/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.kotlin_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv.text  = "hahahaha"
+        btn.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(this@MainActivity, "haha", Toast.LENGTH_LONG).show()
+            }
+
+        })
 
         //*khai bao
 //        var a : String = ""
