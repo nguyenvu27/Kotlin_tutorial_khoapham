3ccfd4e2078b54dd24b5a59aafb949699535472d
Your Name <you@example.com>
2019-08-01 10:30:43 +0700

    update

--- app/src/main/java/com/example/kotlin_1/Main2Activity.kt
+++ app/src/main/java/com/example/kotlin_1/Main2Activity.kt
@@ -2,11 +2,18 @@ package com.example.kotlin_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val intent = intent
+        val hoTen : String = intent.getStringExtra("data")
+        val hoTen2 : Int = intent.getIntExtra("data2", 2000 )
+        tv2.text = hoTen
+        tv3.text = hoTen2.toString()
     }
 }
